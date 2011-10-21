maliit/framework

commit 26e535927e9ba832bd46b20f69709e930eaf97c3

Changes: Move common code/headers to new directory common/
RevBy: Maksim Kirillov

This makes the dependencies between components clearer. For
instance, input-context did not really depend on code in src/
except for a namespace header.

The fact that maliit/maliit.pro would not only handle the libmaliit
headers, pkg-config and similar was also a bit suprising.

common/common.pro
@@ -1,0 +1,30 @@
+include(../config.pri)
+
+TEMPLATE = lib
+TARGET = maliit-common
+
+outputFiles(maliit-framework-$${MALIIT_FRAMEWORK_INTERFACE_VERSION}.pc)
+
+HEADERSINSTALL = \
+    minputmethodnamespace.h \
+
+FRAMEWORKHEADERSINSTALL = \
+    maliit/namespace.h
+
+HEADERS += \
+    $$HEADERSINSTALL \
+    $$FRAMEWORKHEADERSINSTALL \
+
+headers.path += $$M_IM_INSTALL_HEADERS/$$MALIIT_HEADER/maliit
+headers.files += $$HEADERSINSTALL
+
+frameworkheaders.path += $$M_IM_INSTALL_HEADERS/$$MALIIT_FRAMEWORK_HEADER/maliit
+frameworkheaders.files += $$FRAMEWORKHEADERSINSTALL
+
+install_pkgconfig.path = $${M_IM_INSTALL_LIBS}/pkgconfig
+install_pkgconfig.files = $$OUT_PWD/maliit-framework-$${MALIIT_FRAMEWORK_INTERFACE_VERSION}.pc
+
+INSTALLS += \
+    headers \
+    frameworkheaders \
+    install_pkgconfig \

examples/apps/plainqt/plainqt.pro
@@ -21,9 +21,9 @@ contains(BUILD_TYPE, unittest) {
         CONFIG += link_pkgconfig
         PKGCONFIG += maliit-1.0
     } else {
-        TOP_SRC = ../../..
-        INCLUDEPATH += $$TOP_SRC
-        LIBS += $$TOP_SRC/maliit/lib$${MALIIT_LIB}.so
+        TOP_DIR = ../../..
+        INCLUDEPATH += $$TOP_DIR $$TOP_DIR/common
+        LIBS += $$TOP_DIR/maliit/lib$${MALIIT_LIB}.so
     }
 }
 

examples/apps/twofields/twofields.pro
@@ -21,9 +21,9 @@ contains(BUILD_TYPE, unittest) {
         CONFIG += link_pkgconfig
         PKGCONFIG += maliit-1.0
     } else {
-        TOP_SRC = ../../..
-        INCLUDEPATH += $$TOP_SRC
-        LIBS += $$TOP_SRC/maliit/lib$${MALIIT_LIB}.so
+        TOP_DIR = ../../..
+        INCLUDEPATH += $$TOP_DIR $$TOP_DIR/common
+        LIBS += $$TOP_DIR/maliit/lib$${MALIIT_LIB}.so
     }
 }
 

examples/plugins/cxx/helloworld/helloworld.pro
@@ -41,7 +41,7 @@ contains(BUILD_TYPE, unittest) {
 
     SRC_DIR = $$TOP_DIR/src
     LIBS += $$SRC_DIR/lib$${MALIIT_PLUGINS_LIB}.so
-    INCLUDEPATH += $$SRC_DIR
+    INCLUDEPATH += $$SRC_DIR $$TOP_DIR/common
 
     target.path += $$MALIIT_TEST_PLUGINS_DIR/examples/cxx/helloworld
     INSTALLS += target

examples/plugins/cxx/override/override.pro
@@ -41,7 +41,7 @@ contains(BUILD_TYPE, unittest) {
 
     SRC_DIR = $$TOP_DIR/src
     LIBS += $$SRC_DIR/lib$${MALIIT_PLUGINS_LIB}.so
-    INCLUDEPATH += $$SRC_DIR
+    INCLUDEPATH += $$SRC_DIR $$TOP_DIR/common
 
     target.path += $$MALIIT_TEST_PLUGINS_DIR/examples/cxx/override
     INSTALLS += target

maliit/inputcontext-qt4

commit 9d9d245fa23cafde6b577cbece9a724b31a763ee

Fixes: minputmethodnamespace.h installed to wrong directory
RevBy: TrustMe
Regression introduced in 26e535927e9ba832bd46b20f69709e930eaf97c3

Full, original commit at dd8aaa13248ae8bbaed1fefc7b4b723f1f4de081 in maliit-framework

common/.gitignore
@@ -1,0 +1,1 @@
+*.pc

common/common.pro
@@ -15,7 +15,7 @@ HEADERS += \
     $$HEADERSINSTALL \
     $$FRAMEWORKHEADERSINSTALL \
 
-headers.path += $$M_IM_INSTALL_HEADERS/$$MALIIT_HEADER/maliit
+headers.path += $$M_IM_INSTALL_HEADERS/$$MALIIT_PLUGINS_HEADER
 headers.files += $$HEADERSINSTALL
 
 frameworkheaders.path += $$M_IM_INSTALL_HEADERS/$$MALIIT_FRAMEWORK_HEADER/maliit